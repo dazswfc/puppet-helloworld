2b1f1c4b4ee8f5dadac63b2e4b6d82b1a8741727
root <root@rhel7.example.com>
2016-07-31 19:58:47 +0100

    Updated motd to test r10k

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -1,10 +1,16 @@
 class helloworld::motd {
 
-    file { '/etc/motd':
-    owner  => 'root',
-    group  => 'root',
-    mode    => '0644',
-    content => "hello, world!\n",
-    }
+  file { '/etc/motd':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "hello, world!\n",
+  }
 
- }
+  file { '/etc/test':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "This is a test of r10k",
+  }
+}
